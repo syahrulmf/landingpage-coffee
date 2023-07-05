f4e3976ee2c092f743da8eb83af8c2492336a2d8
syahrulmf <syahrulmf@gmail.com>
2023-07-05 11:25:05 +0700

    feat: add search

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-// Toggle class active
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
-
+// ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar element
 const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
